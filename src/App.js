753a5af02b5255fde815bbca6db8d3011bb2afbd
saagor16 <saagor.16@gmail.com>
2025-01-25 11:39:22 +0600

    X button add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TicTacToe from './Component/TicTacToe';
 
 function App() {
   return (
     <div className="App">
-      
+      <TicTacToe></TicTacToe>
     </div>
   );
 }
--- src/Component/TicTacToe.js
+++ src/Component/TicTacToe.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const TicTacToe = () => {
+  const renderSquare = () => {
+    return <button>x</button>;
+  };
   return (
-    <div>
-      
+    <div className="board">
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
+      <div className="board-row">
+        {renderSquare()}
+        {renderSquare()}
+        {renderSquare()}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TicTacToe
+export default TicTacToe;
